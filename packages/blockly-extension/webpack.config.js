4def3745dc94687e14dcd479e6f337504efc1e33
Carlos Herrero <26092748+hbcarlos@users.noreply.github.com>
2023-04-26 21:57:37 +0200

    Ignore blockly source map warnings

--- packages/blockly-extension/webpack.config.js
+++ packages/blockly-extension/webpack.config.js
@@ -1,5 +1,17 @@
 // @ts-check
 
 module.exports = /** @type { import('webpack').Configuration } */ ({
-  devtool: 'source-map'
+  devtool: 'source-map',
+  module: {
+    rules: [
+      // Load Blockly source maps.
+      {
+        test: /(blockly\/.*\.js)$/,
+        use: [require.resolve('source-map-loader')],
+        enforce: 'pre',
+      }
+    ].filter(Boolean),
+  },
+  // https://github.com/google/blockly-samples/blob/9974e85becaa8ad17e35b588b95391c85865dafd/plugins/dev-scripts/config/webpack.config.js#L118-L120
+  ignoreWarnings: [/Failed to parse source map/]
 });
